d9d0cb2880236afd0cf3ec667045754925ca976e
rajan <rajan.rana6@gmail.com>
2025-06-02 03:13:07 +0530

    update page and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">welcome <br />
+    <h1 className="text-2xl text-center">welcome!
     this is my first next application
     <br />
    </h1>
